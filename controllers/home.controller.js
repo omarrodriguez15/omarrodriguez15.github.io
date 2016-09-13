8caa6a5514c07a03fcb6cd065c498d3c3b9804c2
rr2d2 <rodriguezomar91@gmail.com>
2016-09-12 21:46:21 -0500

    -Cleaned up tabs on main tabs -Added image paths to home controller -Added and removed some images

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -3,6 +3,9 @@ angular.module('home.controller', []).
     console.log('made it here');
     var vm = this;
     vm.about = about;
+    vm.contactImgPath = '..\\img\\ethernet.jpg';
+    vm.mainImgPath = '..\\img\\gentTrans.jpg';
+    vm.blogImgPath = '..\\img\\blogIntro.jpg';
 
     return vm;
 
