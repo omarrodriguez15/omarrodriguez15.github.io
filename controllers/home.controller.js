00098b6c71184eccb9573de51e5b45059f10bc2d
rr2d2 <rodriguezomar91@gmail.com>
2016-09-12 22:51:11 -0500

    -Cards are created using a array of card objects with -fixed email thing again -fixed the background image b/c it was fudged up

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -6,6 +6,18 @@ angular.module('home.controller', []).
     vm.contactImgPath = '..\\img\\ethernet.jpg';
     vm.mainImgPath = '..\\img\\gentTrans.jpg';
     vm.blogImgPath = '..\\img\\blogIntro.jpg';
+    vm.blogCards = [
+        {
+          imgPath: '..\\img\\blogIntro.jpg',
+          title:'Intro',
+          content:'This is where I will post summarys of blogs linking to full content eventually... if you\'re bored. Beware it is not organized super well.'
+        },
+        {
+          imgPath:'..\\img\\blogIntro.jpg',
+          title: 'First Entry',
+          content: 'Here is my first entry'
+        }
+      ];
 
     return vm;
 
