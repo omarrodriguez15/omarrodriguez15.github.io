2f301b8b0dbe558af6c7bdc4f6c734cc1cdaaa99
omarrodriguez15 <rodriguezomar91@gmail.com>
2016-11-10 20:48:29 -0600

    Add api call to get real level

--- controllers/coffee.controller.js
+++ controllers/coffee.controller.js
@@ -5,6 +5,7 @@ angular.module('coffee.controller', []).
     var vm = this;
     vm.config = {};
     vm.newValue = newValue;
+    vm.gauge = {};
 
     init();
     return vm;
@@ -21,10 +22,24 @@ angular.module('coffee.controller', []).
       vm.config.waveAnimateTime = 5000;
       vm.config.waveHeight = 0.2;
       vm.config.waveCount = 1;
-      var gauge = loadLiquidFillGauge("fillgauge", 33.33, vm.config);
+      var prodUrl = 'http://jackalapi.azurewebsites.net/api/coffee';
+      $http.get(prodUrl)
+         .then(function(response) {
+            if(!response.data)
+              return "POOP";
+            console.log(response.data[0].level);
+            var level = Math.round((response.data[0].level / 6) * 100);
+            vm.gauge = loadLiquidFillGauge("fillgauge", level, vm.config);
+         });
     }
 
     function newValue(){
-      loadLiquidFillGauge("fillgauge", 55, vm.config);
+      var ran = 0;
+      if(Math.random() > .5){
+        ran = Math.round(Math.random()*100);
+      } else {
+        ran = (Math.random()*100).toFixed(1);
+      }
+      vm.gauge.update(ran);
     }
 });
