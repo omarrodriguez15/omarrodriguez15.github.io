ac16105af461db96e80611109bea089ab6683639
omarrodriguez15 <rodriguezomar91@gmail.com>
2018-10-16 06:36:43 -0500

    Initial adding pdf page count ability

--- pt/main.js
+++ pt/main.js
@@ -1,90 +1,100 @@
 let totalNumberOfPages = 0;
+let filePageCountQueue = [];
 
 document.getElementById('modal-checkout').onclick = modalCheckout;
 document.getElementById('modal-back').onclick = modalBackClicked;
 
 function modalCheckout() {
-    console.log('checkout clicked');
-    updatePriceEstimate(calculatePriceEtimate(totalNumberOfPages));
-    $('#details-modal').modal('toggle');
+   console.log('checkout clicked');
+   updatePriceEstimate(calculatePriceEtimate(totalNumberOfPages));
+   $('#details-modal').modal('toggle');
 }
 
 function modalBackClicked() {
-    console.log('Modal back clicked');
-    updatePriceEstimate(calculatePriceEtimate(totalNumberOfPages));
-    $('#details-modal').modal('toggle');
+   console.log('Modal back clicked');
+   updatePriceEstimate(calculatePriceEtimate(totalNumberOfPages));
+   $('#details-modal').modal('toggle');
 }
 
 function dragOverHandler(e) {
-    console.log('File(s) in drop zone'); 
-    e = e || event;
-    // Prevent default behavior (Prevent file from being opened)
-    e.preventDefault();
+   console.log('File(s) in drop zone');
+   e = e || event;
+   // Prevent default behavior (Prevent file from being opened)
+   e.preventDefault();
 }
 
 function dropHandler(e) {
-    console.log('File(s) dropped');
-    e = e || event;
+   console.log('File(s) dropped');
+   e = e || event;
 
-    // Prevent default behavior (Prevent file from being opened)
-    e.preventDefault();
-    totalNumberOfPages = 50;
+   // Prevent default behavior (Prevent file from being opened)
+   e.preventDefault();
 
-    updatePriceEstimate(calculatePriceEtimate(totalNumberOfPages));
+   if (e.dataTransfer.items) {
+      // Use DataTransferItemList interface to access the file(s)
+      for (var i = 0; i < e.dataTransfer.items.length; i++) {
+         // If dropped items aren't files, reject them
+         if (e.dataTransfer.items[i].kind === 'file') {
+            var file = e.dataTransfer.items[i].getAsFile();
 
-    if (e.dataTransfer.items) {
-        // Use DataTransferItemList interface to access the file(s)
-        for (var i = 0; i < e.dataTransfer.items.length; i++) {
-            // If dropped items aren't files, reject them
-            if (e.dataTransfer.items[i].kind === 'file') {
-                var file = e.dataTransfer.items[i].getAsFile();
-                updateFileList(file.name);
-                console.log('... file[' + i + '].name = ' + file.name);
-            }
-        }
-    } else {
-        // Use DataTransfer interface to access the file(s)
-        for (var i = 0; i < e.dataTransfer.files.length; i++) {
-            console.log('... file[' + i + '].name = ' + e.dataTransfer.files[i].name);
-        }
-    } 
+            filePageCountQueue.push(
+               {
+                  "file": file,
+                  "index": i
+               });
 
-    // Pass event to removeDragData for cleanup
-    // removeDragData(e);
+            var fileReader = new FileReader();
+            fileReader.onload = function (e) {
+               readPDFFile(new Uint8Array(e.target.result));
+            };
+            fileReader.readAsArrayBuffer(file);
+            console.log('... file[' + i + '].name = ' + file.name);
+         }
+      }
+   }
+}
+
+function readPDFFile(pdf) {
+   PDFJS.getDocument({ data: pdf }).then(function (pdf) {
+      console.log(pdf.pdfInfo.numPages);
+      totalNumberOfPages = pdf.pdfInfo.numPages;
+      updateFileList(filePageCountQueue.pop(), totalNumberOfPages);
+      updatePriceEstimate(calculatePriceEtimate(totalNumberOfPages));
+   });
 }
 
 function dragStartHandler(e) {
-    console.log("dragstarthandler");
+   console.log("dragstarthandler");
 }
 
 function calculatePriceEtimate(pageCount) {
-    if (pageCount <= 0) return;
-    let bindingPrice = getBindingPrice();
-    let inkPrice = getInkPrice(pageCount);
-    let pagePrice = getPagePrice();
+   if (pageCount <= 0) return;
+   let bindingPrice = getBindingPrice();
+   let inkPrice = getInkPrice(pageCount);
+   let pagePrice = getPagePrice();
 
-    return bindingPrice + (pageCount * inkPrice) + (pageCount * pagePrice);
+   return bindingPrice + (pageCount * inkPrice) + (pageCount * pagePrice);
 }
 
 function getPagePrice() {
-    return parseInt(document.getElementById('paper').value, 10);
+   return parseInt(document.getElementById('paper').value, 10);
 }
 
 function getBindingPrice() {
-    return parseInt(document.getElementById('binding').value, 10);
+   return parseInt(document.getElementById('binding').value, 10);
 }
 
 function getInkPrice(pageCount) {
-    let inkType = document.getElementById('ink').value;
+   let inkType = document.getElementById('ink').value;
 
-    if (inkType === 'c') {
-        return getColorInkPrice(pageCount);
-    }
-    else if (inkType === 'b') {
-        return getBlackAndWhiteInkPrice(pageCount);
-    }
+   if (inkType === 'c') {
+      return getColorInkPrice(pageCount);
+   }
+   else if (inkType === 'b') {
+      return getBlackAndWhiteInkPrice(pageCount);
+   }
 
-    alert('Invalid ink price');
+   alert('Invalid ink price');
 }
 
 // 1-100    - $0.45
@@ -92,10 +102,10 @@ function getInkPrice(pageCount) {
 // 250-500  - $0.35
 // 500-1000 - $0.30
 function getColorInkPrice(pageCount) {
-    if (pageCount > 500)  return .3;
-    if (pageCount > 250) return .35;
-    if (pageCount > 101) return .39;
-    return .45
+   if (pageCount > 500) return .3;
+   if (pageCount > 250) return .35;
+   if (pageCount > 101) return .39;
+   return .45
 }
 
 // 1-100    - $0.12
@@ -103,45 +113,45 @@ function getColorInkPrice(pageCount) {
 // 250-500  - $0.07
 // 500-1000 - $0.06
 function getBlackAndWhiteInkPrice(pageCount) {
-    if (pageCount > 500)  return .06;
-    if (pageCount > 250) return .07;
-    if (pageCount > 101) return .08;
-    return .12
+   if (pageCount > 500) return .06;
+   if (pageCount > 250) return .07;
+   if (pageCount > 101) return .08;
+   return .12
 }
 
 function updatePriceEstimate(price) {
-    if (!price) return;
-    price = prettyFromatPrice(price);
-    document.getElementById('price-text').innerText = `$${price}`;
+   if (!price) return;
+   price = prettyFromatPrice(price);
+   document.getElementById('price-text').innerText = `$${price}`;
 }
 
-function updateFileList(filename) {
-    document.getElementById('file-names-container').append(buildFileListItem(1, filename));
+function updateFileList(file, pageCount) {
+   document.getElementById('file-names-container').append(buildFileListItem(1, file.file.name, pageCount));
 }
 
-function buildFileListItem(id, fileName){
-    let listItem = document.createElement('li');
-    id = sanatizeString(id);
-    fileName = sanatizeString(fileName);
+function buildFileListItem(id, fileName, pageCount) {
+   let listItem = document.createElement('li');
+   id = sanatizeString(id);
+   fileName = sanatizeString(fileName);
 
-    listItem.id = `${id}-f`;
-    listItem.className = "col-md-10 up-files list-group-item d-flex justify-content-between align-items-center";
-    listItem.innerText = fileName;
+   listItem.id = `${id}-f`;
+   listItem.className = "col-md-10 up-files list-group-item d-flex justify-content-between align-items-center";
+   listItem.innerText = fileName;
 
-    let badge = document.createElement('span');
-    badge.className = "badge badge-info badge-pill";
-    badge.innerText = `${totalNumberOfPages} pages`;
+   let badge = document.createElement('span');
+   badge.className = "badge badge-info badge-pill";
+   badge.innerText = `${pageCount} pages`;
 
-    listItem.append(badge);
+   listItem.append(badge);
 
-    return listItem;
-    //listItem.innerHTML = <i col-md-2 class="far fa-trash-alt circle-icon remove-file"></i>
+   return listItem;
+   //listItem.innerHTML = <i col-md-2 class="far fa-trash-alt circle-icon remove-file"></i>
 }
 
 function prettyFromatPrice(price) {
-    return (price).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+   return (price).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
 }
 
 function sanatizeString(dirtyString) {
-    return dirtyString.toString().replace('<', '&lt;').replace('>', '&gt;');
+   return dirtyString.toString().replace('<', '&lt;').replace('>', '&gt;');
 }
