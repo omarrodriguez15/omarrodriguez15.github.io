826b2d760ec1bf26863415f4776dfcd661bede28
omarrodriguez15 <rodriguezomar91@gmail.com>
2019-05-21 22:41:59 -0500

    Replace old angular website with new generated site via jekyll

--- js/disqus.js
+++
@@ -1,6 +0,0 @@
-(function() { // DON'T EDIT BELOW THIS LINE
-   // var d = document, s = d.createElement('script');
-   // s.src = '//rr2d2.disqus.com/embed.js';
-   // s.setAttribute('data-timestamp', +new Date());
-   // (d.head || d.body).appendChild(s);
-})();
